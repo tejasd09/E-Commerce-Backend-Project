fc69ac7c5acb5d9b5613190e2ce4b3069d206e78
tejasd09 <tejas1372003@gmail.com>
2025-04-08 12:03:16 +0530

    Delete E-Commerce directory

--- E-Commerce/src/main/java/com/ecommerce/repository/ProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-}
